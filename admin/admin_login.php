17ba5028b3a8d263f51be771f2facefcfe01d82c
samiunniloy <60150274+samiunniloy@users.noreply.github.com>
2022-03-18 23:47:34 +0600

    Create admin_login.php

---
+++ admin/admin_login.php
@@ -0,0 +1,51 @@
+<?php
+session_start();
+include("../Html/conect.php");
+?>
+<div class="main">
+    <div class="wrapper">
+        <h1>Teacher log in</h1>
+        <br>
+        <br>
+        <form action=""method="POST">
+            <table class="add-admin">
+                
+                <tr>
+                    <td>Name:</td>
+                    <td><input type="text"name="email" placeholder="Enter your email"></td>
+                </tr>
+                <td>Password:</td>
+                   <td> <input type="password" name="password"placeholder="Enter Password"></td>
+                
+                <tr>
+                    <td colspan="2">
+                        <input type="submit" name="submit" value="LOg in" class="btn-second">
+
+                    </td>
+                </tr>
+                
+            </table>
+        </form>
+    </div>
+</div>
+
+
+<?php
+    if(isset($_POST['submit'])){
+        $email=$_POST['email'];
+        $password=md5($_POST['password']);
+        $sql="SELECT * FROM teacher where name='$email' AND password='$password'";
+        $res=mysqli_query($con,$sql);
+        $count=mysqli_num_rows($res);
+        if($count==1){
+            $_SESSION['login']="<div class='success'>Login Successful.</div>";
+            $_SESSION['user']=$name;
+            header('location:index.php');
+        }
+        else{
+            $_SESSION['login']="<div class='error text-center'>Name or Password didn't match.</div>";
+            header('location:admin_login.php');
+        }
+    }
+
+?>
