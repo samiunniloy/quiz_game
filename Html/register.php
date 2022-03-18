ebe62d1898a4dc70eb55b796ee1d78c20a3c8a12
samiunniloy <60150274+samiunniloy@users.noreply.github.com>
2022-03-18 19:50:59 +0600

    login-register
    log in register a back end ses ekhn front end kor

---
+++ Html/register.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Online Quiz </title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <link rel="stylesheet" href="../css/style.css">
+
+</head>
+<body>
+    <div class="log_in ">
+        <br><br>
+        <h1 class="t_center" >LOG IN</h1>
+        <br>
+         <form action="" method="POST" class="t_center">
+             User Name : <br><br>
+             <input type="text" name="username" placeholder="Enter Username"> <br><br>
+             Password : <br><br>
+             <input type="password" name="password" placeholder="Enter Password"> <br><br>
+ 
+             <input type="submit" value="Log In"  name="submit" class="btn">
+             <input type="submit" value="Register"  name="submit" class="btn">
+             
+         </form>
+     </div>
+</body>
+</html>
