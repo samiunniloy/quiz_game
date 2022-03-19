ad9beafe8486d1fb24b28d1800ffeebe860fbbf0
ZRJOY10 <67340615+ZRJOY10@users.noreply.github.com>
2022-03-20 03:34:31 +0600

    Create header.php

---
+++ admin/header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Online Quiz </title>
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="../css/style.css">
+
+</head>
+
+<body>
+
+    <!-- header section starts  -->
+
+    <header class="header1 ">
+
+        <a href="#" class="logo">
+            <img src="../images/logo.png" alt="">
+        </a>
+
+        <nav class="navbar">
+            <a href="http://localhost/quiz_game-main/index.php">Visite Site</a>
+            <a href=".\Admin_profile.php">Admin</a>
+            <a href=".\make_quiz.php">Quiz</a>
+            <a href=".\add_ques.php"> Question</a>
+            <a href="..\Html\contact.html">Log Out</a>
+        </nav>
+
+        <div class="icons">
+            <div class="fas fa-bars" id="menu-btn"></div>
+        </div>
+
+
+    </header>
