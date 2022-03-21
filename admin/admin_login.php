dd873977ac99c69a5f5f6734cc5ed8e53f1a8b85
samiunniloy <60150274+samiunniloy@users.noreply.github.com>
2022-03-22 02:25:09 +0600

    Update admin_login.php

--- admin/admin_login.php
+++ admin/admin_login.php
@@ -1,49 +1,61 @@
+
 <?php
+
 session_start();
 include("../Html/conect.php");
-include("./header.php");
+
 ?>
-<section class="mar25">
-<div class="log_in ">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Online Quiz </title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <link rel="stylesheet" href="../css/style.css">
+
+</head>
+<body>
+    <div class="log_in ">
         <br><br>
-        <h1 class="t_center" >Add Question</h1>
-        <br>  
-        <form action=""method="POST" class="t_center">
-            
-                
-                
-                    Name: <br>
-                    <input type="text"name="email" placeholder="Enter your email"><br>
-              
-                Password:<br>
-                    <input type="password" name="password"placeholder="Enter Password"><br>
-                
-                
-                        <input type="submit" name="submit" value="Log in" class="btn">
+        <h1 class="t_center" >LOG IN</h1>
+        <br>
+         <form action="" method="POST" class="t_center">
 
-                    <br>
-
-        </form>
-    </div>
-</section>
-
-
+         <!-- Name : <br><br>
+             <input type="text" name="username" placeholder="Enter Username"> <br><br>
+             Institute : <br><br>
+             <input type="text" name="institute" placeholder="Enter Institute"> <br><br> -->
+            Name : <br><br>
+             <input type="text" name="Name" placeholder="Enter Name"> <br><br>
+             Password : <br><br>
+             <input type="password" name="password" placeholder="Enter Password"> <br><br>
+ 
+             <input type="submit" value="Log In"  name="login" class="btn">
+             <!-- <a href="register.php" class="btn">Sign-Up</a> -->
+             
+         </form>
+     </div>
+</body>
+</html>
 <?php
-    if(isset($_POST['submit'])){
-        $email=$_POST['email'];
+ if(isset($_POST['login'])){ 
+      $name=$_POST['Name'];
         $password=($_POST['password']);
-        $sql="SELECT * FROM teacher where name='$email' AND password='$password'";
-        $res=mysqli_query($con,$sql);
-        $count=mysqli_num_rows($res);
-        if($count==1){
-            $_SESSION['login']="<div class='success'>Login Successful.</div>";
-            $_SESSION['id']=$sno;
-            header('location:index.php');
-        }
-        else{
-            $_SESSION['login']="<div class='error text-center'>Name or Password didn't match.</div>";
-            header('location:admin_login.php');
-        }
+   $sql="SELECT * FROM `teacher` WHERE name='$name' && password='$password'";
+      $res=mysqli_query($con,$sql);
+      if(mysqli_num_rows($res)>0){
+          header('location:index.php');
+      }
+   else{
+//        //  $_SESSION['login']="<div class='error text-center'>Name or Password didn't match.</div>";
+      header('location:admin_login.php');
     }
 
-?>
+   }
+
+
+  ?>
